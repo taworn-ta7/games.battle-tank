b826817d65f907aea0953926821af951a72e4b5f
Taworn Ta <taworn.ta7@gmail.com>
2022-04-04 22:28:01 +0700

    - add constants and use them instead raw

---
+++ Battle Tank Preview/constants.gd
@@ -0,0 +1,18 @@
+extends Node
+
+# กรอบฉากต่อสู้
+const AREA_LEFT = 0
+const AREA_RIGHT = 600
+const AREA_UP = 0
+const AREA_DOWN = 600
+
+# ทิศทาง
+const DIRECTION_LEFT = 0
+const DIRECTION_RIGHT = 1
+const DIRECTION_UP = 2
+const DIRECTION_DOWN = 3
+
+# ขนาดรถถัง
+const TANK_WIDTH = 64
+const TANK_HEIGHT = 64
+
--- Battle Tank Preview/tank.gd
+++ Battle Tank Preview/tank.gd
@@ -1,56 +1,70 @@
 extends Area2D
 
+# รูป
 var base_image = 0
 var sprite_left = [base_image + 2, base_image + 4]
 var sprite_right = [base_image + 6, base_image + 8]
 var sprite_up = [base_image + 0, base_image + 2]
 var sprite_down = [base_image + 4, base_image + 6]
 var sprite = sprite_up
-var velocity = Vector2(0, 0)
-var speed = 3
+var move = false
+var direction = Constants.DIRECTION_UP
 
-# Called when the node enters the scene tree for the first time.
+# ความเร็ว
+var velocity = Vector2(0, 0)
+var speed = 4
+
+# เรียกเมื่อ สร้าง Scene
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# เรียกเรื่อยๆ ทุกเวลา delta ผ่านไป
 func _process(delta):
 	# Check input 4 directions.
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -1
 		velocity.y = 0
 		sprite = sprite_left
+		move = true
+		direction = Constants.DIRECTION_LEFT
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = 1
 		velocity.y = 0
 		sprite = sprite_right
+		move = true
+		direction = Constants.DIRECTION_RIGHT
 	elif Input.is_action_pressed("ui_up"):
 		velocity.x = 0
 		velocity.y = -1
 		sprite = sprite_up
+		move = true
+		direction = Constants.DIRECTION_UP
 	elif Input.is_action_pressed("ui_down"):
 		velocity.x = 0
 		velocity.y = 1
 		sprite = sprite_down
+		move = true
+		direction = Constants.DIRECTION_DOWN
 	else:
 		velocity.x = 0
 		velocity.y = 0
+		move = false
 		$Sprite.playing = false
 		return
 
 	# check boundary
-	if velocity.x <= 0 && position.x <= 0:
+	if velocity.x <= 0 && position.x <= Constants.AREA_LEFT:
 		velocity.x = 0
 		position.x = 0
-	if velocity.x >= 0 && position.x >= 1024 - 64:
+	if velocity.x >= 0 && position.x >= Constants.AREA_RIGHT - Constants.TANK_WIDTH:
 		velocity.x = 0
-		position.x = 1024 - 64
-	if velocity.y <= 0 && position.y <= 0:
+		position.x = Constants.AREA_RIGHT - Constants.TANK_WIDTH
+	if velocity.y <= 0 && position.y <= Constants.AREA_UP:
 		velocity.y = 0
 		position.y = 0
-	if velocity.y >= 0 && position.y >= 600 - 64:
+	if velocity.y >= 0 && position.y >= Constants.AREA_DOWN - Constants.TANK_HEIGHT:
 		velocity.y = 0
-		position.y = 600 - 64
+		position.y = Constants.AREA_DOWN - Constants.TANK_HEIGHT
 
 	# If has input, move.
 	if $Sprite.frame < sprite[0] || $Sprite.frame >= sprite[-1]:
@@ -60,6 +74,7 @@ func _process(delta):
 		$Sprite.playing = true
 	translate(velocity * speed)
 
+# เรียกเมื่อ animation frame เปลี่ยน
 func _on_Sprite_frame_changed():
 	if $Sprite.frame >= sprite[-1]:
 		$Sprite.frame = sprite[0]
